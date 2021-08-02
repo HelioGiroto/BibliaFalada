3846b64bd2ce2c35392f44303a64c7340cc3425d
Helio Giroto <heliogiroto76@gmail.com>
2021-08-02 15:15:32 -0300

    Peq ajustes no CSS dos 3 bts "outros gráficos"

--- graficos.js
+++ graficos.js
@@ -24,11 +24,11 @@ function fatiaTempo(valor) {
 
 function totalTempoOuvido(valor) {
     // chama função:
-    let valorOuvido = fatiaTempo(valor)
-    // imprime:
-    document.querySelector("#horasOuvido").innerHTML = valorOuvido[0]
-    document.querySelector("#minutosOuvido").innerHTML = valorOuvido[1]
-    document.querySelector("#segundosOuvido").innerHTML = valorOuvido[2]
+    let tempoFatiado = fatiaTempo(valor)
+    
+    // imprime qto tempo já ouvir hoje em ('#tempoOuvido'):
+    let impressao = imprimeFatiado(tempoFatiado)
+    document.querySelector('#tempoOuvido').innerHTML = `${impressao[0]}${impressao[1]}${impressao[2]}`
 }
 
 
@@ -37,18 +37,16 @@ function faltaOuvir(valor) {
     let faltaOuvirCapitulos = 1190 - totalCapOuvidos
     document.querySelector('#faltaOuvirCapitulos').innerHTML = faltaOuvirCapitulos
 
-    // chama função:
     // valor é todo o tempo ouvido + o tempo da fx atual
     // falta Ouvir Total é tudo o que o usuário falta ouvir de toda a Bíblia.
     // 83 horas (total de horas dos 1190 audios) * 3600 segundos:
     let faltaOuvirTotal = (83 * 3600) - valor
-    let valorFalta = fatiaTempo(faltaOuvirTotal)
-    console.log(valorFalta)
+    let tempoFatiado = fatiaTempo(faltaOuvirTotal)
 
-    // imprime:
-    document.querySelector("#horasFaltam").innerHTML = valorFalta[0]
-    document.querySelector("#minutosFaltam").innerHTML = valorFalta[1]
-    document.querySelector("#segundosFaltam").innerHTML = valorFalta[2]
+
+    // imprime qto tempo já ouvir hoje em ('#tempoFalta'):
+    let impressao = imprimeFatiado(tempoFatiado)
+    document.querySelector('#tempoFalta').innerHTML = `${impressao[0]}${impressao[1]}${impressao[2]}`
 }
 
 
@@ -67,17 +65,9 @@ function quantoOuviuHoje() {
     // vai buscar os valores de hora, minuto e segundo de total de segundos (qtoOuviuHoje)
     let tempoFatiado = fatiaTempo(qtoOuviuHoje)
 
-    // se não chega a 60 minutos:
-    // let horas = Math.trunc(qtoOuviuHoje / 3600)
-    if (tempoFatiado[0] === 0) {
-        tempoFatiado[0] = ''
-    } else {
-        tempoFatiado[0] = `${tempoFatiado[0]}h, `
-    }
-
-    // imprime qto tempo já ouvir hoje em ('#totalDiario'):
-    document.querySelector('#totalDiario').innerHTML = `${tempoFatiado[0]}${tempoFatiado[1]} min e ${tempoFatiado[2]} seg.`
-
+    // imprime qto tempo em ('#totalDiario'):
+    let impressao = imprimeFatiado(tempoFatiado)
+    document.querySelector('#totalDiario').innerHTML = `${impressao[0]}${impressao[1]}${impressao[2]}`
 }
 
 
@@ -87,11 +77,20 @@ function totalCapitulosOuvidos() {
     document.querySelector("#totalCapitulos").innerHTML = totalCapOuvidos
 }
 
+function imprimeFatiado(lista) {
+    // caso hora, min ou seg seja = 0, omite o 0:
+    if (lista[0] === 0) {lista[0] = ''} else {lista[0] = `${lista[0]} hs, `}
+    if (lista[1] === 0) {lista[1] = ''} else {lista[1] = `${lista[1]} min `}
+    if (lista[2] === 0) {lista[2] = ''} else {lista[2] = `e ${lista[2]} seg`}
+
+    let novaLista = [lista[0], lista[1], lista[2]]
+    return novaLista
+}
 
 function calculaMetricas() {
     // obtem tempo de audicao do local Storage:
     tempoAudicao = Math.trunc(Number(localStorage.tempoAudicao))
-    
+
     // obtem o tempo corrente da fx que o usuário está ouvindo:
     tempoFaixaAtual = Math.trunc(player.currentTime)
 
@@ -103,5 +102,4 @@ function calculaMetricas() {
 
     totalTempoOuvido(totalTempo)
     faltaOuvir(totalTempo)
-
 }
